07a45d9b2b4a7870116d37dd1e74e4211e4ec01e
Raymond Hammarling <raymond@hammarlings.se>
2016-04-02 23:25:03 +0200

    Implement reading TAP and updating AnyBar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// The entry point of all to come...
+#!/usr/bin/env node
+
+import parser from 'tap-parser';
+import anybar from 'anybar';
+import minimist from 'minimist';
+
+const argv = minimist(process.argv.slice(2), {
+    default: { port: 1738 },
+    alias: { p: 'port' }
+});
+
+anybar('orange', { port: argv.port });
+
+process.stdin.pipe(process.stdout);
+
+process.stdin.pipe(parser(results => {
+    anybar(results.ok ? 'green' : 'exclamation', { port: argv.port });
+}));
